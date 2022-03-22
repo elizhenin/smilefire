92baf93f0c9ad27818ef36f6ee672be0c376cf6c
elizhenin <elizhenin@gmail.com>
2022-03-22 14:39:14 +0300

    share setup

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,63 +1,67 @@
 <div style="text-align: center; ">
-  <span class="copyright_text">
-    <b>SmileFire 2007-<?php echo date('Y') ?></b><br/>
-    <!--<a href="karta-sajta" style="font-size:8pt;">Карта сайта</a><br/>-->
-  </span>
+    <span class="copyright_text">
+        <b>SmileFire 2007-<?php echo date('Y') ?></b><br />
+        <!--<a href="karta-sajta" style="font-size:8pt;">Карта сайта</a><br/>-->
+    </span>
     <?php
     //counters begin
     ?>
-    <br/>
-    <script type="text/javascript" src="//yandex.st/share/share.js"
-            charset="utf-8"></script>
-    <div class="yashare-auto-init" data-yashareL10n="ru"
-         data-yashareQuickServices="yaru,vkontakte,facebook,twitter,odnoklassniki,moimir" data-yashareTheme="counter"
-
-        ></div>
-    <br/>
+    <br />
+    <script type="text/javascript" src="//yandex.st/share/share.js" charset="utf-8"></script>
+    <div class="yashare-auto-init" data-yashareL10n="ru" data-yashareQuickServices="yaru,vkontakteodnoklassniki,moimir" data-yashareTheme="counter"></div>
+    <br />
     <!-- Yandex.Metrika informer -->
-    <a href="/away.php?url=http://metrika.yandex.ru/stat/?id=23719822&amp;from=informer"
-       target="_blank" rel="nofollow"><img src="//bs.yandex.ru/informer/23719822/3_0_202020FF_000000FF_1_pageviews"
-                                           style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:23719822,lang:'ru'});return false}catch(e){}"/></a>
+    <a href="/away.php?url=http://metrika.yandex.ru/stat/?id=23719822&amp;from=informer" target="_blank" rel="nofollow"><img src="//bs.yandex.ru/informer/23719822/3_0_202020FF_000000FF_1_pageviews" style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:23719822,lang:'ru'});return false}catch(e){}" /></a>
     <!-- /Yandex.Metrika informer -->
 
     <!-- Yandex.Metrika counter -->
     <script type="text/javascript">
-        (function (d, w, c) {
+        (function(d, w, c) {
             (w[c] = w[c] || []).push(function() {
                 try {
-                    w.yaCounter23719822 = new Ya.Metrika({id:23719822,
-                        webvisor:true,
-                        clickmap:true,
-                        trackLinks:true,
-                        accurateTrackBounce:true});
-                } catch(e) { }
+                    w.yaCounter23719822 = new Ya.Metrika({
+                        id: 23719822,
+                        webvisor: true,
+                        clickmap: true,
+                        trackLinks: true,
+                        accurateTrackBounce: true
+                    });
+                } catch (e) {}
             });
 
             var n = d.getElementsByTagName("script")[0],
                 s = d.createElement("script"),
-                f = function () { n.parentNode.insertBefore(s, n); };
+                f = function() {
+                    n.parentNode.insertBefore(s, n);
+                };
             s.type = "text/javascript";
             s.async = true;
             s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
 
             if (w.opera == "[object Opera]") {
                 d.addEventListener("DOMContentLoaded", f, false);
-            } else { f(); }
+            } else {
+                f();
+            }
         })(document, window, "yandex_metrika_callbacks");
     </script>
-    <noscript><div><img src="//mc.yandex.ru/watch/23719822" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <noscript>
+        <div><img src="//mc.yandex.ru/watch/23719822" style="position:absolute; left:-9999px;" alt="" /></div>
+    </noscript>
     <!-- /Yandex.Metrika counter -->
-    <!--LiveInternet counter--><script type="text/javascript"><!--
-        document.write("<a href='/away.php?url=http://www.liveinternet.ru/click' "+
-        "target=_blank><img src='//counter.yadro.ru/hit?t15.6;r"+
-        escape(document.referrer)+((typeof(screen)=="undefined")?"":
-        ";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
-            screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
-        ";"+Math.random()+
-        "' alt='' title='LiveInternet: показано число просмотров за 24"+
-        " часа, посетителей за 24 часа и за сегодня' "+
-        "border='0' width='88' height='31'><\/a>")
-        //--></script><!--/LiveInternet-->
+    <!--LiveInternet counter-->
+    <script type="text/javascript">
+        document.write("<a href='/away.php?url=http://www.liveinternet.ru/click' " +
+            "target=_blank><img src='//counter.yadro.ru/hit?t15.6;r" +
+            escape(document.referrer) + ((typeof(screen) == "undefined") ? "" :
+                ";s" + screen.width + "*" + screen.height + "*" + (screen.colorDepth ?
+                    screen.colorDepth : screen.pixelDepth)) + ";u" + escape(document.URL) +
+            ";" + Math.random() +
+            "' alt='' title='LiveInternet: показано число просмотров за 24" +
+            " часа, посетителей за 24 часа и за сегодня' " +
+            "border='0' width='88' height='31'><\/a>")
+    </script>
+    <!--/LiveInternet-->
 
     <!--
     <a href="http://www.prchecker.info/" title="Free Page Rank Tool" target="_blank">
